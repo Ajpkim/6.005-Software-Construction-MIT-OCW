c6963c7d19ccd6b1245181d4f89707df333de260
Alexander <alexkim345@gmail.com>
2020-01-11 12:50:48 -0500

    Finished gravity cacl

--- JavaBasics/src/gravityCalculator/GravityCalculator.java
+++ JavaBasics/src/gravityCalculator/GravityCalculator.java
@@ -6,13 +6,19 @@ public class GravityCalculator {
         double initialVelocity = 0.0;
         double fallingTime = 10.0;
         double initialPosition = 0.0;
-        double finalPosition = 0.0;
+       
+        double accelTime = gravity * Math.pow(fallingTime, 2);
+        
+        double finalPosition = (0.5 * accelTime) 
+                + (initialVelocity * fallingTime)
+                + initialPosition;
+                
          System.out.println("The object's position after " + fallingTime +
         " seconds is " + finalPosition + " m.");
-        
+
         }
         
 }
 
 // Before modifying: "The object's position after 10.0 seconds is 0.0 m."
-// After: ""
+// After: "The object's position after 10.0 seconds is -490.5 m."
